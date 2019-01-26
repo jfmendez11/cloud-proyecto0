e601271b5d45d89ec348b02bca3a7d6c70affeaf
jfmendez11 <jf.mendez11@uniandes.edu.co>
2019-01-26 15:21:08 -0500

    Vainas del back
    Intento de autenticación

--- models/users.js
+++ models/users.js
@@ -1,10 +1,42 @@
 /* Global Require */
 const mongoose =  require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UsersSchema = new mongoose.Schema({
     email: {type: String, unique: true, required: true, trim: true},
     password: {type: String, required: true,},
+});
+
+UsersSchema.statics.authenticate = (email, password, callback) => {
+    User.findOne({ email: email })
+    .exec((err, user) => {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    });
+  }
+
+UsersSchema.pre('save', (next) => {
+    var user = this;
+    bcrypt.hash(user.password, 10, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
 });
 
 module.exports = mongoose.model('Users', UsersSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,72 @@
 var express = require('express');
-var router = express.Router();
+const bodyParser = require('body-parser');
+const Users = require('../models/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Vamos los millos');
-});
-
-module.exports = router;
+module.exports = (app, express) => {
+  let apiRouter = express.Router();
+  apiRouter.route('/')
+  .post((req, res) => {
+    if (req.body.password !== req.body.passwordConf) {
+      var err = new Error('Passwords do not match.');
+      err.status = 400;
+      res.send("passwords dont match");
+      return next(err);
+    }
+    if(req.body.email && req.body.password && req.body.passwordConf) {
+      let user = new Users();
+      user.email = req.body.email;
+      user.password =  req.body.password;
+      user.save((err) => {
+        if(err) return res.send(err);
+        return res.json({message: 'Usuario creado existosamente'});
+      });
+    } else if (req.body.logemail && req.body.logpassword) {
+      User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+        if (error || !user) {
+          var err = new Error('Wrong email or password.');
+          err.status = 401;
+          return next(err);
+        } else {
+          req.session.userId = user._id;
+          return res.redirect('/profile');
+        }
+      });
+    } else {
+      var err = new Error('All fields required.');
+      err.status = 400;
+      return next(err);
+    }
+  });
+  apiRouter.route('/profile') 
+  .get((req, res, next) => {
+    Users.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        if (user === null) {
+          var err = new Error('Not authorized! Go back!');
+          err.status = 400;
+          return next(err);
+        } else {
+          //return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
+          res.send('a ver esto que manda');
+        }
+      }
+    });
+  });
+  apiRouter.route('/logout')
+  .get((req, res, next) => {
+    if (req.session) {
+      // delete session object
+      req.session.destroy(function (err) {
+        if (err) {
+          return next(err);
+        } else {
+          return res.redirect('/');
+        }
+      });
+    }
+  }); 
+  return apiRouter;
+}
